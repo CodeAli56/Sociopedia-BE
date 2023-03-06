3d36ff22b86668def2e676c5c7cac0aac235001f
CodeAli56 <codeali56@gmail.com>
2023-03-06 13:26:51 +0530

    first commit

--- index.js
+++ index.js
@@ -1,45 +1,66 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const userModel = require('./models/user');
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
+import { verifyToken } from "./middleware/auth.js";
+import User from "./models/User.js";
+import Post from "./models/Posts.js";
+import { users, posts } from "./data/index.js";
 
+/* CONFIGURATIONS */
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
 const app = express();
-
-app.use(cors());
 app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
-mongoose.connect('mongodb://localhost:27017/db',
-  { useNewUrlParser: true, useUnifiedTopology: true }
-);
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+/* FILE STORAGE */
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
 });
+const upload = multer({ storage });
 
-app.post('/user/create', async (req, res) => {
-  const { username } = req.body;
+/* ROUTES WITH FILES */
+app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
-  // Create a new user in the database
-  const newUser = await userModel.create({
-    username,
-  });
+/* ROUTES */
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
-  res.json({
-    message: 'User created successfully',
-    data: newUser,
-  });
-});
-
-app.get('/users', async (req, res) => {
-  // Get all users from the database
-  const users = await userModel.find({});
-
-  res.json({
-    message: 'Users fetched successfully',
-    data: users,
+/* MONGOOSE SETUP */
+const PORT = process.env.SERVER_PORT || 6001;
+mongoose.set("strictQuery" , true);
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   })
-});
-
-app.listen(4000, () => {
-  console.log('Server started on port 4000');
-});
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
